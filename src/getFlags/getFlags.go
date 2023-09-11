fa7dfa2cf780d89aafde3ce0a6e6313ca34552bb
Ellis Lee <ellismckenzielee@gmail.com>
2023-09-11 20:37:24 +0100

    add aws modules

--- src/getFlags/getFlags.go
+++ src/getFlags/getFlags.go
@@ -3,6 +3,9 @@ package main
 import (
         "context"
         "github.com/aws/aws-lambda-go/lambda"
+        "github.com/aws/aws-sdk-go-v2/service/dynamodb"
+        "github.com/aws/aws-sdk-go/aws/session"
+
 )
 
 type Response struct {
@@ -10,6 +13,7 @@ type Response struct {
 }
 
 func HandleRequest(ctx context.Context ) (Response, error) {
+        sess, err := session.NewSession()
         response := Response{statusCode: 200}
         return response, nil
 }
